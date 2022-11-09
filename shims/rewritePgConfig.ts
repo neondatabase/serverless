adf00bbeded51bef1ad1db62fe2306bc0a93306a
George MacKerron <george@mackerron.co.uk>
2022-11-09 16:52:06 +0000

    Now adding ssl=true automatically for *.neon.tech DB hosts

--- shims/rewritePgConfig.ts
+++ shims/rewritePgConfig.ts
@@ -15,12 +15,10 @@ export default function rewritePgConfig(config: any) {
     let newConfig = config;
     if (typeof newConfig === 'string') newConfig = parse(newConfig);
     if (newConfig.connectionString) newConfig = Object.assign({}, newConfig, parse(newConfig.connectionString));
-    delete newConfig.connectionString;  // because this still has the unrewritten password and would be re-parsed by pg
+    delete newConfig.connectionString;  // because this still has the bare password and would be re-parsed by pg
 
     const host = newConfig.host ?? process.env.PGHOST ?? process.env.host;
-
-    // apply this only to Neon hosts
-    if (typeof host === 'string' && /[.]neon[.]tech(:|$)/.test(host)) {
+    if (typeof host === 'string' && /[.]neon[.]tech(:|$)/.test(host)) {  // apply changes only to Neon hosts
       const projectMatch = host.match(/^([^.]+)[.]/);
       if (projectMatch !== null) {
         const project = projectMatch[1];
@@ -28,6 +26,7 @@ export default function rewritePgConfig(config: any) {
         if (typeof originalPassword === 'string') {
           // console.log('SCRAM bypass implemented');
           newConfig.password = `project=${project};${originalPassword}`;
+          newConfig.ssl = true;  // turn on SSL, since Neon won't connect without it
           config = newConfig;
         }
       }
