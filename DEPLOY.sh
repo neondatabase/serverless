260b6dc372c031ffc5bc18ae88f816859734d001
George MacKerron <george@mackerron.co.uk>
2023-03-16 09:39:00 +0000

    Improved wsproxy deployment instructions

--- DEPLOY.sh
+++ DEPLOY.sh
@@ -1,66 +1,46 @@
-
 # To deploy wsproxy behind nginx (for TLS) on a host ws.example.com
-# running Ubuntu 22.04 (and Postgres locally), you'd do something like
-# the following:
+# running Ubuntu 22.04 (and Postgres locally), you'd do something similar to
+# the following.
 
-sudo su  # we do the rest as root
+# note: be sure port 443 isn't firewalled off
 
-apt install golang nginx certbot python3-certbot-nginx
+# upgrade to 22.04 if on earlier version (required for recent enough Golang)
 
-echo '127.0.0.1 ws.example.com' >> /etc/hosts
+sudo su  # do this all as root
 
-echo '                                                                          
-server {
-  listen 80;
-  listen [::]:80;
-  server_name ws.example.com;
-  location / {
-    proxy_pass http://127.0.0.1:6543/;
-    proxy_set_header Upgrade $http_upgrade;
-    proxy_set_header Connection "Upgrade";
-    proxy_set_header Host $host;
-  }
-}
-' > /etc/nginx/sites-available/wsproxy   
+apt update -y && apt upgrade -y && apt dist-upgrade -y
+apt autoremove -y && apt autoclean -y
+apt install -y update-manager-core
+do-release-upgrade  # and answer yes to all defaults
 
-ln -s /etc/nginx/sites-available/wsproxy /etc/nginx/sites-enabled/wsproxy
+# ... reboots, then ...
 
-certbot --nginx -d ws.example.com
+sudo su  # we do the rest as root too
 
-echo '
-server {
-  server_name ws.example.com;
+export HOSTDOMAIN=ws.example.com  # edit the domain name for your case
 
-  location / {
-    proxy_pass http://127.0.0.1:6543/;
-    proxy_set_header Upgrade $http_upgrade;
-    proxy_set_header Connection "Upgrade";
-    proxy_set_header Host $host;
-  }
+# if required: install postgres + create a password-auth user
 
-  listen [::]:80 ipv6only=on;
-  listen 80;
+apt install -y postgresql
 
-  listen [::]:443 ssl ipv6only=on; # managed by Certbot
-  listen 443 ssl; # managed by Certbot
-  ssl_certificate /etc/letsencrypt/live/ws.example.com/fullchain.pem; # managed by Certbot
-  ssl_certificate_key /etc/letsencrypt/live/ws.example.com/privkey.pem; # managed by Certbot
-  include /etc/letsencrypt/options-ssl-nginx.conf; # managed by Certbot
-  ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem; # managed by Certbot
-}
-' > /etc/nginx/sites-available/wsproxy
+echo 'create database wstest; create user wsclear; grant all privileges on database wstest to wsclear;' | sudo -u postgres psql
+sudo -u postgres psql  # and run: \password wsclear
 
-service nginx restart
+perl -pi -e 's/^# IPv4 local connections:\n/# IPv4 local connections:\nhost all wsclear 127.0.0.1\/32 password\n/' /etc/postgresql/14/main/pg_hba.conf
+service postgresql restart
+
+# install wsproxy
 
 adduser wsproxy --disabled-login
 
 sudo su wsproxy
+cd
 git clone https://github.com/neondatabase/wsproxy.git
 cd wsproxy
 go build
 exit
 
-echo '
+echo "
 [Unit]
 Description=wsproxy
 
@@ -69,13 +49,61 @@ Type=simple
 Restart=always
 RestartSec=5s
 User=wsproxy
-Environment=LISTEN_PORT=:6543 ALLOW_ADDR_REGEX='^ws.example.com:5432$'
+Environment=LISTEN_PORT=:6543 ALLOW_ADDR_REGEX='^${HOSTDOMAIN}:5432\$'
 ExecStart=/home/wsproxy/wsproxy/wsproxy
 
 [Install]
 WantedBy=multi-user.target
-' > /lib/systemd/system/wsproxy.service
+" > /lib/systemd/system/wsproxy.service
 
 systemctl enable wsproxy
 service wsproxy start
 
+# install nginx as tls proxy
+
+apt install -y golang nginx certbot python3-certbot-nginx
+
+echo "127.0.0.1 ${HOSTDOMAIN}" >> /etc/hosts
+
+echo "                                                                       
+server {
+  listen 80;
+  listen [::]:80;
+  server_name ${HOSTDOMAIN};
+  location / {
+    proxy_pass http://127.0.0.1:6543/;
+    proxy_set_header Upgrade \$http_upgrade;
+    proxy_set_header Connection Upgrade;
+    proxy_set_header Host \$host;
+  }
+}
+" > /etc/nginx/sites-available/wsproxy   
+
+ln -s /etc/nginx/sites-available/wsproxy /etc/nginx/sites-enabled/wsproxy
+
+certbot --nginx -d ${HOSTDOMAIN}
+
+echo "
+server {
+  server_name ${HOSTDOMAIN};
+
+  location / {
+    proxy_pass http://127.0.0.1:6543/;
+    proxy_set_header Upgrade \$http_upgrade;
+    proxy_set_header Connection Upgrade;
+    proxy_set_header Host \$host;
+  }
+
+  listen [::]:80 ipv6only=on;
+  listen 80;
+
+  listen [::]:443 ssl ipv6only=on; # managed by Certbot
+  listen 443 ssl; # managed by Certbot
+  ssl_certificate /etc/letsencrypt/live/${HOSTDOMAIN}/fullchain.pem; # managed by Certbot
+  ssl_certificate_key /etc/letsencrypt/live/${HOSTDOMAIN}/privkey.pem; # managed by Certbot
+  include /etc/letsencrypt/options-ssl-nginx.conf; # managed by Certbot
+  ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem; # managed by Certbot
+}
+" > /etc/nginx/sites-available/wsproxy
+
+service nginx restart
