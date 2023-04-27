c31a659b014756d5311113414a6374368c01f1f2
George MacKerron <george@mackerron.co.uk>
2023-04-27 17:16:59 +0100

    Fixed process.nextTick shim for Vercel/Next.js

--- dist/node/index.mjs
+++ dist/node/index.mjs
@@ -1,8 +1,11 @@
 // note: subtls has non-deterministic failures using webcrypto on node
-// import { webcrypto } from 'crypto';
-// globalThis.crypto ??= webcrypto;
+import { webcrypto } from 'crypto';
+globalThis.crypto ??= webcrypto;
 
-const { latencies } = await import('../serverless.mjs');  // dynamic import picks up globalThis.crypto value, when static import doesn't
+import ws from 'ws';
+globalThis.WebSocket = ws;
+
+const { latencies } = await import('../serverless.mjs');  // dynamic import picks up globalThis values, when static import doesn't
 const log = (s) => process.stdout.write(s.replace(/<[^>]+>/g, ''));
 
 latencies(process.env, false, log);  // false -> no subtls tests
--- shims/shims.js
+++ shims/shims.js
@@ -1,16 +1,29 @@
 
 export const global = globalThis;
-export const setImmediate = globalThis.setImmediate ?? (fn => setTimeout(fn, 0));
 
-// Next.js appears to set up a booby-trapped Buffer object that just springs errors on us, so test allocUnsafe as well
+export const setImmediate = globalThis.setImmediate ?? (fn => setTimeout(fn, 0));
+export const clearImmediate = globalThis.clearImmediate ?? (id => clearTimeout(id));
+
+export const crypto = globalThis.crypto ?? {};
+crypto.subtle ??= {};
+
+// --- Buffer ---
+
+// Next.js has a booby-trapped Buffer object that just springs errors, so we test allocUnsafe as well
 export const Buffer =
   typeof globalThis.Buffer === 'function' && typeof globalThis.Buffer.allocUnsafe === 'function' ?
     globalThis.Buffer :
     require('buffer/').Buffer;
 
-export const process = globalThis.process ?? {};
-process.env ??= {};
-process.nextTick ??= fn => setTimeout(fn, 0);
 
-export const crypto = globalThis.crypto ?? {};
-crypto.subtle ??= {};
+// --- process ---
+
+export let process = globalThis.process ?? {};
+process.env ??= {};
+
+try { process.nextTick(() => void 0); }
+catch (err) {
+  // if we got here, either nextTick is not defined, or it's booby-trapped to throw (e.g. in Next.js), so ...
+  const resolve = Promise.resolve();
+  process.nextTick = resolve.then.bind(resolve);
+}
