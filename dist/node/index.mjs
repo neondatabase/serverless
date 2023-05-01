8b7f68da5556349c42f7d2565113fa824c7e6fad
George MacKerron <george@mackerron.co.uk>
2023-05-01 17:20:12 +0100

    Updated packages, including bugfixed subtls

--- dist/node/index.mjs
+++ dist/node/index.mjs
@@ -8,4 +8,4 @@ globalThis.WebSocket = ws;
 const { latencies } = await import('../serverless.mjs');  // dynamic import picks up globalThis values, when static import doesn't
 const log = (s) => process.stdout.write(s.replace(/<[^>]+>/g, ''));
 
-latencies(process.env, false, log);  // false -> no subtls tests
+latencies(process.env, true, log);  // true/false -> run subtls tests or not
