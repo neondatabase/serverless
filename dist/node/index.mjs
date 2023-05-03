9350761d759e0e462223e373121014d5fa0e7250
George MacKerron <george@mackerron.co.uk>
2023-05-03 13:36:16 +0100

    Tweaks

--- dist/node/index.mjs
+++ dist/node/index.mjs
@@ -3,6 +3,7 @@ import { webcrypto } from 'crypto';
 globalThis.crypto ??= webcrypto;
 
 import ws from 'ws';
+// import { WebSocket as ws } from 'undici';  // waiting for release of https://github.com/nodejs/undici/pull/2106
 globalThis.WebSocket = ws;
 
 const { latencies } = await import('../serverless.mjs');  // dynamic import picks up globalThis values, when static import doesn't
