2e10e97f7d7681a4ace04548c9508c21d84fe8af
Stas Kelvich <stas.kelvich@gmail.com>
2024-04-15 15:21:11 +0300

    Pass username and dbname through url decoder.
    Postgres protocol expects raw usernames and dbnames, so users with non-url-safe
    characters in their username can't properly authorize.

--- shims/url/index.ts
+++ shims/url/index.ts
@@ -4,6 +4,8 @@ export function parse(url: string, parseQueryString = false) {
   const httpUrl = 'http:' + url.substring(protocol.length);
   let { username, password, host, hostname, port, pathname, search, searchParams, hash } = new URL(httpUrl);
   password = decodeURIComponent(password);
+  username = decodeURIComponent(username);
+  pathname = decodeURIComponent(pathname);
   const auth = username + ':' + password;
   const query = parseQueryString ? Object.fromEntries(searchParams.entries()) : search;
   return { href: url, protocol, auth, username, password, host, hostname, port, pathname, search, query, hash };
