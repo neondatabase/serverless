85f390baf90a320ee54a3cbb820da53f9f87f483
George MacKerron <george@mackerron.co.uk>
2024-12-18 18:34:37 +0000

    format:fix

--- tests/ws.ts
+++ tests/ws.ts
@@ -2,7 +2,9 @@ import { neonConfig } from '../dist/npm';
 
 export async function shimWebSocket() {
   if (typeof WebSocket !== 'function') {
-    console.info('Native WebSocket not found: importing ws for neonConfig.webSocketConstructor');
+    console.info(
+      'Native WebSocket not found: importing ws for neonConfig.webSocketConstructor',
+    );
     const { WebSocket } = await import('ws');
     neonConfig.webSocketConstructor = WebSocket;
   } else {
