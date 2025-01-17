933b25fc90829544dedff1c297a376cf2339bd48
George MacKerron <george@mackerron.co.uk>
2025-01-17 19:16:57 +0000

    Authorize test working

--- tests/cli/httpAuthorize.test.ts
+++ tests/cli/httpAuthorize.test.ts
@@ -1,12 +1,9 @@
 import { expect, test, beforeAll, assertType } from 'vitest';
 import { neon } from '../../dist/npm';
 import { polyfill } from './polyfill';
-import { createClerkClient } from '@clerk/backend';
 
 const DATABASE_URL = process.env.VITE_NEON_DB_URL!;
 const CLERK_USER = process.env.VITE_CLERK_USER!;
-const CLERK_PASSWORD = process.env.VITE_CLERK_PASSWORD!;
-const CLERK_PK = process.env.VITE_CLERK_PUBLISHABLE_KEY!;
 const CLERK_SECRET_KEY = process.env.VITE_CLERK_SECRET_KEY!;
 
 beforeAll(polyfill);
@@ -23,7 +20,7 @@ async function clerkAPI(method: 'POST' | 'GET', endpoint: string, body?: any) {
   return response.json();
 }
 
-test('connections with Neon Authorize', async () => {
+test('connection with Neon Authorize', async () => {
   const [{ id: user_id }] = await clerkAPI(
     'GET',
     `users?email_address[]=${CLERK_USER}`,
@@ -31,15 +28,13 @@ test('connections with Neon Authorize', async () => {
   const { id: session_id } = await clerkAPI('POST', 'sessions', { user_id });
   const { jwt } = await clerkAPI('POST', `sessions/${session_id}/tokens`);
 
-  console.log(jwt);
-
   const sql = neon(DATABASE_URL);
   await sql`CREATE EXTENSION IF NOT EXISTS pg_session_jwt`;
 
   const authSql = neon(DATABASE_URL.replace(/[^/]+@/, 'authenticated@'), {
     authToken: jwt,
   });
-  const queriedUserId = await authSql`SELECT auth.user_id()`;
+  const [{ user_id: queried_user_id }] = await authSql`SELECT auth.user_id()`;
 
-  console.log(queriedUserId);
+  expect(queried_user_id).toBe(user_id);
 });
