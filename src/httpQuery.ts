9156de65e71ffb459f160fc450a6a22ae0dae27d
Luis Neves <luisneves@neon.tech>
2025-04-23 11:50:29 +0100

    Adds console disclaimer when running direct sql from the browser

--- src/httpQuery.ts
+++ src/httpQuery.ts
@@ -365,6 +365,22 @@ export function neon<
         headers['Neon-Batch-Deferrable'] = String(resolvedDeferrable);
     }
 
+    const isBrowser =
+      typeof window !== 'undefined' && typeof document !== 'undefined';
+    if (isBrowser) {
+      console.warn(`          
+          ************************************************************
+          *                                                          *
+          *  WARNING: Running SQL directly from the browser can have *
+          *  security implications. While your database is protected *
+          *  by Row-Level Security (RLS), use it at your own risk.   *
+          *  This approach is great for fast prototyping, but ensure *
+          *  proper safeguards are in place to prevent misuse or     *
+          *  execution of expensive SQL queries by your end users.   *
+          *                                                          *
+          ************************************************************`);
+    }
+
     // --- run query ---
 
     let response;
